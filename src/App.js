4a9f715a2003aa35cb62a52cf52e216e0c453652
Enes Şahin <enes.cyclones@gmail.com>
2022-02-14 00:14:04 +0300

    Text Generator App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect, useState} from "react";
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text,setText] = useState("");
+    const [selection,setSelection] = useState("HTML")
+    const [paragraphs, setParagraphs] = useState(4);
+
+
+    useEffect(() => {
+        let URL = process.env.REACT_APP_API_BASE_URL+process.env.REACT_APP_PARAGRAPHS+paragraphs+process.env.REACT_APP_FORMAT+selection;
+        axios.get(URL).then(res => setText(res.data))
+    },[paragraphs]);
+
+
+
+    return (
+
+        <div className="flex-col flex justify-center items-center  w-full h-full bg-slate-700 min-h-screen">
+            <h1 className={"text-4xl text-white text-center font-bold mt-32 "}>Welcome The Text Generator</h1>
+            <div className={"flex flex-col justify-center items-center h-full lg:w-1/2 px-6 py-6"}>
+                <div className={"flex flex-col"}>
+                    <form>
+                        <input onChange={e=> setParagraphs(Number(e.target.value))} type={"number"} min={0} className={"px-4 py-2 m-4  p-4 placeholder:text-emerald-500 self-start"}
+                               placeholder={"Choose Number"}/>
+                        <label htmlFor={"format"} className={"px-2 py-2 text-emerald-500"}>Choose a Format: </label>
+                        <select onChange={e=> setSelection(e.target.value)} name="format" className={"px-4 py-2 bg-white text-emerald-500 m-4 p-4 self-end"}>
+                            <option>
+                                HTML
+                            </option>
+                            <option>
+                                Text
+                            </option>
+                        </select>
+                    </form>
+
+                </div>
+                <div className={"w-full h-full"}>
+                    {selection==="HTML" ? text.map(p => <text className={"text-emerald-500 text-md w-full" }> {"<p>"+p+"</p>"}</text>) : <text className={"text-lg text-emerald-500 "}>{text}</text> }
+
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
